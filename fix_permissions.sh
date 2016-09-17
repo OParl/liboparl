0e9cd4cafe75845710ba66346ee08abb019387e6
grindhold <grindhold@gmx.net>
2016-09-18 00:34:38 +0200

    generating docs

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -7,3 +7,4 @@ chmod 644 /usr/local/lib/girepository-1.0/OParl-0.2.typelib
 # via release.
 mv /usr/local/lib/x86_64-linux-gnu/pkgconfig/oparl-0.2.pc /usr/local/lib/pkgconfig/
 chmod 644 /usr/local/lib/pkgconfig/oparl-0.2.pc
+chmod -R 644 /usr/local/share/devhelp/books/oparl-0.2
