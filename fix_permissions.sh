d7cc159d9f22928731c5618f712f18eb583d2637
grindhold <grindhold@gmx.net>
2016-09-18 18:06:45 +0200

    correctly set permissions for devhelp book

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -8,3 +8,5 @@ chmod 644 /usr/local/lib/girepository-1.0/OParl-0.2.typelib
 mv /usr/local/lib/x86_64-linux-gnu/pkgconfig/oparl-0.2.pc /usr/local/lib/pkgconfig/
 chmod 644 /usr/local/lib/pkgconfig/oparl-0.2.pc
 chmod -R 644 /usr/local/share/devhelp/books/oparl-0.2
+chmod 755 /usr/local/share/devhelp/books/oparl-0.2
+chmod 755 /usr/local/share/devhelp/books/oparl-0.2/img
